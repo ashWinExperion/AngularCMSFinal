fbf8aec7bc6a57770c4af6c4fbecf0fbaa665765
Ashwin Madhu <ashwin.madhu@experionglobal.local>
2022-02-15 10:15:14 +0530

    2nd day

--- src/app/lab-tech/list-test-today/list-test-today.component.ts
+++ src/app/lab-tech/list-test-today/list-test-today.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AppointmentService } from 'src/app/shared/appointment.service';
 
 @Component({
   selector: 'app-list-test-today',
@@ -9,23 +7,9 @@ import { AppointmentService } from 'src/app/shared/appointment.service';
 })
 export class ListTestTodayComponent implements OnInit {
 
-  listAppointments;
-  constructor(private appointmentService:AppointmentService,
-    private router:Router) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.appointmentService.getAllApointmentsForAdoctor(6).subscribe(
-      result=>{
-        this.listAppointments=result;
-        console.log(result);
-      }
-    )
-  }
-
-  consult(appId)
-  {
-    this.router.navigate(["/doctor/consulting",appId]);
-    alert(appId);
   }
 
 }
